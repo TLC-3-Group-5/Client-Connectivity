102585048e9d9b59381df25fd06a627732441e8d
AlhajiTaibu <alhabdutaib@gmail.com>
2021-03-23 16:34:52 +0000

    Changes made

--- src/main/java/io/turntabl/producer/resources/service/PortfolioService.java
+++ src/main/java/io/turntabl/producer/resources/service/PortfolioService.java
@@ -5,6 +5,7 @@ import io.turntabl.producer.resources.model.OwnedStock;
 import io.turntabl.producer.resources.model.Portfolio;
 import io.turntabl.producer.resources.model.Response;
 import io.turntabl.producer.resources.repository.ClientRepository;
+import io.turntabl.producer.resources.repository.OwnedStockRepository;
 import io.turntabl.producer.resources.repository.PortfolioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,11 +17,15 @@ import java.util.List;
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
     private final ClientRepository clientRepository;
+    private final OwnedStockRepository ownedStockRepository;
 
     @Autowired
-    public PortfolioService(PortfolioRepository portfolioRepository, ClientRepository clientRepository) {
+    public PortfolioService(PortfolioRepository portfolioRepository,
+                            ClientRepository clientRepository,
+                            OwnedStockRepository ownedStockRepository) {
         this.portfolioRepository = portfolioRepository;
         this.clientRepository= clientRepository;
+        this.ownedStockRepository = ownedStockRepository;
     }
 
     // Create a portfolio
@@ -50,6 +55,18 @@ public class PortfolioService {
         return portfolio != null ? portfolio.getClient().getBalance() : null;
     }
 
+    // Update Client's Balance
+    public void updateClientBalance(Long portfolioId, Double valueOfOrder){
+        Portfolio portfolio = this.portfolioRepository.findById(portfolioId).orElse(null);
+
+        if(portfolio!=null){
+            Client client = portfolio.getClient();
+            Double Oldbalance = client.getBalance();
+            client.setBalance(Oldbalance-valueOfOrder);
+            clientRepository.save(client);
+        }
+    }
+
     // Get Client's Stocks on a particular portfolio
     public List<OwnedStock> getStocksOnPortfolio(Long portfolioId){
         Portfolio portfolio = portfolioRepository.findById(portfolioId).orElse(null);
@@ -58,7 +75,20 @@ public class PortfolioService {
 
     // Get Portfolio
     public Portfolio getPortfolio(Long portfolioId){
-        Portfolio portfolio = portfolioRepository.findById(portfolioId).orElse(null);
-        return portfolio;
+        return portfolioRepository.findById(portfolioId).orElse(null);
+    }
+
+    // Update OwnedStock
+    public void updateClientStock(Long portfolioId, String product, Integer quantity){
+        List<OwnedStock> ownedStocks = getStocksOnPortfolio(portfolioId);
+        OwnedStock stock = ownedStocks.stream()
+                .filter(stock1 ->stock1.getTicker().equals(product))
+                .findFirst().orElse(null);
+
+        if(stock!=null){
+            Integer oldQuantity = stock.getQuantity();
+            stock.setQuantity(oldQuantity-quantity);
+            ownedStockRepository.save(stock);
+        }
     }
 }
