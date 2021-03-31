ea1344c1e43f617c01209b5be7121d32bba25f26
kyfs-tt <78965733+kyfs-tt@users.noreply.github.com>
2021-03-31 13:13:55 +0000

    Revert "Implementation of profit and loss"

--- src/main/java/io/turntabl/producer/ClientConnectivityApplication.java
+++ src/main/java/io/turntabl/producer/ClientConnectivityApplication.java
@@ -10,23 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @RestController
 public class ClientConnectivityApplication {
-
-	@Bean
-	private static RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 
 	public static void main(String[] args) {
 
